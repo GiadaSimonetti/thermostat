bceb0da98a18ef1d0a09071169c0fe4048be6b62
philb56 <phil.bugden@outlook.com>
2018-01-31 14:17:14 +0000

    More feature testing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -12,9 +12,35 @@ describe('Feature test', function(){
     expect(thermostat.temperature()).toEqual(20);
   });
 
+  it('sets power saving ON by default', function(){
+    expect(thermostat.powerSavingMode()).toEqual(true);
+  });
+
   it('sets the temperature to 22 °C', function(){
     thermostat.setTemperature(22);
     expect(thermostat.temperature()).toEqual(22);
+  });
+
+  it('can\'t set the temperature below 10 °C', function(){
+    expect(function(){ thermostat.setTemperature(9); }).toThrowError('Minimum temperature reached');
+  });
+
+  it('sets the temperature up 1°C using up()', function(){
+    thermostat.up();
+    expect(thermostat.temperature()).toEqual(21);
+  });
+
+  it('sets the temperature down 1°C using down()', function(){
+    console.log(thermostat)
+    thermostat.down();
+    expect(thermostat.temperature()).toEqual(19);
+    console.log('A');
+  });
+
+  it('raises an error if the temperature is being set < 10°C', function(){
+    thermostat.setTemperature(10);
+    expect(function(){ thermostat.down(); }).toThrowError('Minimum temperature reached');
+    expect(thermostat.temperature()).toEqual(10);
   });
 
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,12 +2,36 @@
 
 function Thermostat(){
   this._temperature = 20;
+  this._powerSavingMode = true;
 }
 
 Thermostat.prototype.temperature = function () {
   return this._temperature;
 };
 
+Thermostat.prototype.powerSavingMode = function () {
+  return this._powerSavingMode;
+};
+
 Thermostat.prototype.setTemperature = function (temperature) {
+  this.minimumTemperature(temperature);
   this._temperature = temperature;
+};
+
+Thermostat.prototype.up = function () {
+  this._temperature += 1;
+};
+
+Thermostat.prototype.down = function () {
+  this.minimumTemperature(this._temperature-1);
+  // if(this._temperature <= 10) {
+  //   throw new Error('Minimum temperature reached');
+  // }
+  this._temperature -= 1;
+};
+
+Thermostat.prototype.minimumTemperature = function (temperature) {
+  if(temperature < 10) {
+    throw new Error('Minimum temperature reached');
+  }
 };
